e05576545bd71da3d9830832bb654c68f5c9ae2a
Daniel Gómez-Sánchez <daniel.gomez.sanchez@hotmail.es>
2018-02-01 09:34:58 +0100

    Add more tests

--- src/test/java/org/magicdgs/http/jsr203/HttpAbstractFileSystemProviderUnitTest.java
+++ src/test/java/org/magicdgs/http/jsr203/HttpAbstractFileSystemProviderUnitTest.java
@@ -1,15 +1,26 @@
 package org.magicdgs.http.jsr203;
 
+import org.mockito.internal.util.collections.Sets;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
+import java.nio.file.AccessDeniedException;
+import java.nio.file.AccessMode;
 import java.nio.file.FileSystemAlreadyExistsException;
 import java.nio.file.FileSystemNotFoundException;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.OpenOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.ProviderMismatchException;
+import java.nio.file.StandardOpenOption;
 import java.util.Collections;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * @author Daniel Gomez-Sanchez (magicDGS)
@@ -79,6 +90,81 @@ public class HttpAbstractFileSystemProviderUnitTest extends BaseTest {
         // test that the new Path is not the same, but equal to the other
         Assert.assertNotSame(actual, expected);
         assertEqualsPath(actual, expected);
+    }
+
+    @DataProvider
+    public Object[][] invalidUris() {
+        return new Object[][] {
+                // null URI
+                {null, IllegalArgumentException.class},
+                // URI without authority (e.g., file URI)
+                {new File("/example.txt").toURI(), IllegalArgumentException.class},
+                // URI with different scheme
+                {URI.create("ftp://example.org/file.txt"), ProviderMismatchException.class}
+        };
+    }
+
+    @Test(dataProvider = "invalidUris")
+    public void testInvalidUris(final URI uri, Class<? extends Throwable> expectedException) throws Exception {
+        Assert.assertThrows(expectedException, () -> new HttpFileSystemProvider().getPath(uri));
+    }
+
+    @DataProvider
+    public Object[][] invalidArgsForByteChannel() {
+        final HttpFileSystemProvider http = new HttpFileSystemProvider();
+        final HttpsFileSystemProvider https = new HttpsFileSystemProvider();
+        final HttpPath httpPath = http.getPath(URI.create("http://example.org/file.txt"));
+        return new Object[][] {
+                // null path or options
+                {http, null, Collections.emptySet(), IllegalArgumentException.class},
+                {http, httpPath, null, IllegalArgumentException.class},
+                // mismatching Path
+                {http, https.getPath(URI.create("https://example.org/file.txt")), Collections.emptySet(), ProviderMismatchException.class},
+                // non existent file
+                {http, httpPath, Collections.emptySet(), NoSuchFileException.class},
+                // UNSUPPORTED BYTE CHANNELS (e.g., writing)
+                // if only an option that it is not supported
+                {http, httpPath, Collections.singleton(StandardOpenOption.WRITE), UnsupportedOperationException.class},
+                // if two options, even if the supported one is requested
+                {http, httpPath, Sets.newSet(StandardOpenOption.READ, StandardOpenOption.APPEND), UnsupportedOperationException.class}
+        };
+    }
+
+    @Test(dataProvider = "invalidArgsForByteChannel")
+    public void testInvalidByteChannels(final HttpAbstractFileSystemProvider provider, Path path,
+            final Set<OpenOption> options,final Class<? extends Throwable> expectedException)
+            throws Exception {
+        Assert.assertThrows(expectedException, () -> provider.newByteChannel(path, options));
+    }
+
+    @DataProvider
+    public Object[][] deniedAccess() {
+        return new Object[][] {
+            {AccessMode.EXECUTE},
+            {AccessMode.WRITE}
+        };
+    }
+
+    @Test(dataProvider= "deniedAccess", expectedExceptions = AccessDeniedException.class)
+    public void testCheckAccessDenied(final AccessMode deniedAcces) throws Exception {
+        final HttpsFileSystemProvider https = new HttpsFileSystemProvider();
+        final HttpPath path = https.getPath(getGithubPagesFileUrl("file1.txt").toURI());
+        https.checkAccess(path, deniedAcces);
+    }
+
+    @Test
+    public void testCheckAccessRead() throws Exception {
+        final HttpsFileSystemProvider https = new HttpsFileSystemProvider();
+        final HttpPath path = https.getPath(getGithubPagesFileUrl("file1.txt").toURI());
+        // this shouldn't throw
+        https.checkAccess(path, AccessMode.READ);
+    }
+
+    @Test(expectedExceptions = NoSuchFileException.class)
+    public void testCheckAccessNoSuchFile() throws Exception {
+        final HttpsFileSystemProvider https = new HttpsFileSystemProvider();
+        final HttpPath path = https.getPath(getGithubPagesFileUrl("no_exists.txt").toURI());
+        https.checkAccess(path, AccessMode.READ);
     }
 
     // test provider for unsupported operations
